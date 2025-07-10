beb8e5838f43a1130effcb2e662e14488c824a98
PrimeZ <the.primez.dev@gmail.com>
2025-07-10 15:50:04 +0330

    improved app statup and documentation page handling

--- app/main.py
+++ app/main.py
@@ -2,51 +2,28 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 import uvicorn
+import os
 
-from app.auth.auth_controller import router as login
-from app.routers.mainadmin_router.main_admin_dash import router as main_admin_dash
-from app.routers.admin_router.admin_dash import router as admin_dash
-from app.routers.admin_router.admin_post import router as admin_post
-from app.routers.mainadmin_router.panel_post import router as panel_post
-from app.routers.mainadmin_router.panel_get import router as panel_get
-from app.routers.mainadmin_router.admin_post import router as mainadmin_post
-from app.routers.mainadmin_router.admin_get import router as mainadmin_get
-from app.routers.mainadmin_router.news_post import router as news_post
-from app.routers.mainadmin_router.news_get import router as news_get
-from app.routers.pruchase_router.plan_post import router as purchase_post
-from app.routers.pruchase_router.plan_get import router as purchase_get
-from app.routers.pruchase_router.payment_post import router as payment_post
-from app.routers.pruchase_router.payment_get import router as payment_get
-from app.routers.mainadmin_router.backup_restore import router as backup_restore
-from app.routers.pruchase_router.extopay_payment import router as extopay_payment
+from app.routers import all_routers
 from app.middleware import RedirectUnauthorizedMiddleware
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env")
+load_dotenv(dotenv_path)
 
 
+DOCS = "/docs" if os.getenv("DOCS").lower() == "true" else None
 
-app = FastAPI()
+app = FastAPI(docs_url=DOCS)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/data/receipts", StaticFiles(directory="data/receipts"), name="receipts")
 app.add_middleware(RedirectUnauthorizedMiddleware)
 
-app.include_router(login)
-app.include_router(main_admin_dash)
-app.include_router(mainadmin_get)
-app.include_router(mainadmin_post)
-app.include_router(admin_dash)
-app.include_router(admin_post)
-app.include_router(panel_get)
-app.include_router(panel_post)
-app.include_router(news_post)
-app.include_router(news_get)
-app.include_router(purchase_post)
-app.include_router(purchase_get)
-app.include_router(payment_post)
-app.include_router(payment_get)
-app.include_router(backup_restore)
-app.include_router(extopay_payment)
+
+for router in all_routers:
+    app.include_router(router)
 
 
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
