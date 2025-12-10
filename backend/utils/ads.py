4cb6a558bb24cb24c3b49ef5c350c902f1afd693
primeZdev <the.primez.dev@gmail.com>
2025-12-10 15:41:11 +0330

    feat(ads): added get ads from github

---
+++ backend/utils/ads.py
@@ -0,0 +1,17 @@
+import requests
+
+
+def get_ads_from_github() -> dict:
+    url = "https://raw.githubusercontent.com/whale-panel/master/media/ads.json"
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        ads_data = response.json()
+        return ads_data
+    except (requests.RequestException, ValueError):
+        return {
+            "title": "جایگاه آگهی شما",
+            "text": "کسب‌وکار خود را به بقیه افراد معرفی کنید! اینجا می‌توانید تبلیغ ویژه خود را قرار دهید",
+            "link": "https://t.me/primezdev",
+            "button": "رزرو جایگاه آکهی",
+        }
